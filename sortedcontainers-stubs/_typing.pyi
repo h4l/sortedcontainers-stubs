8abee5197456b54728fbfe5dc894e38855b013ae
Hal Blackburn <hwtb2@cam.ac.uk>
2023-10-28 04:02:10 +0000

    chore: fix pyright warning
    It doesn't like us importing a private TypeVar from _typeshed.

--- sortedcontainers-stubs/_typing.pyi
+++ sortedcontainers-stubs/_typing.pyi
@@ -1,8 +1,10 @@
 from collections.abc import Hashable
-from typing import Any, Protocol
+from typing import Any, Protocol, TypeVar
 from typing_extensions import TypeAlias
 
-from _typeshed import SupportsDunderGT, SupportsDunderLT, _T_contra  # noqa: F401
+from _typeshed import SupportsDunderGT, SupportsDunderLT
+
+_T_contra = TypeVar("_T_contra", contravariant=True)
 
 class SupportsHashableAndDunderGT(
     Hashable, SupportsDunderGT[_T_contra], Protocol[_T_contra]
